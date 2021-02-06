5d2dd22a639f39391e7fd04134e57c7a72972fe4
ANDRoid7890 <andrey.android7890@gmail.com>
2021-02-06 23:30:01 +0400

    Initial import

---
+++ PKGBUILD
@@ -0,0 +1,30 @@
+# Maintainer: ANDRoid7890 <andrey.android7890@gmail.com>
+
+pkgname=bootsplash-manager
+pkgver=1.0
+pkgrel=1
+pkgdesc="Simple cli bootsplash manager"
+url="https://github.com/ANDRoid7890/bootsplash-manager"
+arch=(x86_64)
+license=(GPL2)
+depends=('qt5-base'
+         'bootsplash-systemd')
+makedepends=(cmake)
+
+source=("CMakeLists.txt"
+        "main.cpp"
+        "org.manjaro.bootsplash-manager.policy")
+
+md5sums=('c15c5c3dbb5d60868db08caaf63ff901'
+         '89c8a891294a4e9ab7cfc6ae8e58c7b8'
+         'bff11696553f09c850aee775a83ba4d3')
+
+build() {
+    cmake .
+    make
+}
+
+package() {
+    install -Dm755 bootsplash-manager "$pkgdir/usr/bin/bootsplash-manager"
+    install -Dm644 org.manjaro.bootsplash-manager.policy "$pkgdir/usr/share/polkit-1/actions/"
+}
---
+++ main.cpp
@@ -0,0 +1,361 @@
+#include <QCoreApplication>
+#include <stdio.h>
+#include <QCommandLineParser>
+#include <QTextStream>
+#include <QFile>
+#include <QDir>
+#include <QRegularExpression>
+#include <unistd.h>
+#include <QProcess>
+
+int help();
+int setTheme( QString theme );
+int setLog();
+int disable();
+int list();
+int status();
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QTextStream out( stdout );
+
+    if ( argc>3 ){
+        out << "too many arguments\n\n";
+        return help();
+    }
+    if ( argc==2 ){
+        if ( strcmp( argv[1], "--status"  ) == 0 )    return status();
+
+        if ( strcmp( argv[1], "-l"        ) == 0
+          || strcmp( argv[1], "--list"    ) == 0 )    return list();
+
+        if ( strcmp( argv[1], "-d"        ) == 0
+          || strcmp( argv[1], "--disable" ) == 0 )    return disable();
+
+        if ( strcmp( argv[1], "--set-log" ) == 0 )    return setLog();
+
+        if ( strcmp( argv[1], "-h"        ) == 0
+          || strcmp( argv[1], "--help"    ) == 0 )    return help();
+    }
+    if ( argc==3 ){
+        if ( strcmp( argv[1], "-s"        ) == 0
+          || strcmp( argv[1], "--set"     ) == 0 )    return setTheme( argv[2] );
+    }
+    out << "unknown command\n\n";
+    return help();
+}
+
+int help(){
+    QTextStream out( stdout );
+    out << "Available commands:\n"
+        << " -h, " << "--help    "      << "\t\t" << "Show this message\n"
+        << " -l, " << "--list    "      << "\t\t" << "List installed themes\n"
+        << " -s, " << "--set <theme_name>"<< "\t" << "Enable bootsplash if it's not, change current theme,\n"
+                                                     <<"\t\t\t   e.g. for \"bootsplash-theme-manjaro\" correct <theme_name> will be \"manjaro\"\n"
+        << " -d, " << "--disable "      << "\t\t" << "Disable bootsplash\n"
+                   << "     --set-log " << "\t\t" << "Disable bootsplash, \"quiet\" option will not be added. You will see log during boot\n"
+                   << "     --status  " << "\t\t" << "Check if bootsplash is enabled and supported by kernel; show selected theme\n";
+
+    return 0;
+}
+
+int setTheme( QString theme ){
+    QTextStream out( stdout );
+
+    if ( !QDir( "/usr/lib/firmware/bootsplash-themes/"+theme ).exists() ) {
+        out << "Incorrect theme name \""+theme+"\"\n";
+        return 0;
+    }
+    if ( !( getuid()==0 ) ){
+        out << "you should use this command with sudo\n";
+        return 0;
+    }
+
+    QStringList data;
+
+    // check grub
+    QFile grub( "/etc/default/grub" );
+    if ( !grub.open( QFile::ReadWrite | QFile::Text ) )
+        return -1;
+    data = QString( grub.readAll() ).split( "\n" );
+
+    QString cmdline;
+    int position = data.indexOf( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT=.*") );
+    cmdline = data.at( position );
+
+    QStringList cmdlineList = cmdline.replace( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT="), "" )
+                                     .replace( "\"", "" )
+                                     .split( ' ' );
+    cmdlineList.removeAll( "quiet" );
+
+    bool bootfileFlag=0;
+    for ( int i=0; i<cmdlineList.size(); i++ ){
+        if ( cmdlineList.at( i ).contains( "bootsplash.bootfile" ) ){
+            cmdlineList.replace( i, "bootsplash.bootfile=/bootsplash-themes/"+theme+"/bootsplash" );
+            bootfileFlag = 1;
+            break;
+        }
+    }
+    if ( !bootfileFlag ) cmdlineList.append( "bootsplash.bootfile=/bootsplash-themes/"+theme+"/bootsplash" );
+
+    cmdline.clear();
+    cmdline="GRUB_CMDLINE_LINUX_DEFAULT=\"";
+    for ( const QString& s : qAsConst(cmdlineList) ) cmdline.append( s ).append( ' ' );
+    cmdline.replace( QRegularExpression( "\\s$" ), "\"");
+
+    data.replace( position, cmdline );
+
+    // write grub
+    out << "Writing GRUB...\nbackup will be saved to /etc/default/grub.bak\n\n";
+    out.flush();
+
+    system( "cp /etc/default/grub /etc/default/grub.bak" );
+
+    grub.resize( 0 );
+    for ( const QString& s : qAsConst(data) ) grub.write( ( s + '\n' ).toUtf8() );
+    grub.close();
+
+    out << "Updating GRUB...\n\n";
+    out.flush();
+    system("grub-mkconfig -o /boot/grub/grub.cfg");
+
+    data.clear();
+
+    // check initcpio
+
+    QDir d( "/usr/lib/firmware/bootsplash-themes/" );
+    d.setFilter( QDir::NoDotAndDotDot | QDir::Dirs );
+    if ( d.count() == 0 ) return -1;
+    QStringList themesList = d.entryList();
+
+    QFile initcpio( "/etc/mkinitcpio.conf" );
+    if ( !initcpio.open( QFile::ReadWrite | QFile::Text ) )
+        return -1;
+    data = QString( initcpio.readAll() ).split("\n");
+
+    QString hooks;
+    position = data.indexOf( QRegularExpression("^HOOKS=.*") );
+    hooks = data.at( position );
+
+    QStringList hooksList = hooks.replace( QRegularExpression("^HOOKS="), "" )
+                                 .replace('"', "")
+                                 .split(' ');
+    bool hooksFlag=1;
+    for ( const QString& t : qAsConst(themesList) )
+        if ( !hooksList.contains( "bootsplash-"+t ) )
+            hooksFlag=0;
+
+    if ( !hooksFlag ){
+        auto last_it = std::remove_if( hooksList.begin(),
+                                       hooksList.end(),
+                                       [](const QString& s){return s.contains("bootsplash-");} );
+        hooksList.erase(last_it, hooksList.end());
+
+        for ( const QString& t : qAsConst(themesList) )
+            hooksList.append( "bootsplash-"+t );
+        hooks.clear();
+        hooks="HOOKS=\"";
+        for ( const QString& s : qAsConst(hooksList) ) hooks.append( s ).append( ' ' );
+        hooks.replace( QRegularExpression( "\\s$" ), "\"" );
+        data.replace( position, hooks );
+
+        // write initcpio
+        out << "Writing initcpio...\nbackup will be saved to /etc/mkinitcpio.conf.bak\n\n";
+        out.flush();
+
+        system( "cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak" );
+
+        initcpio.resize( 0 );
+        for ( const QString& s : qAsConst(data) ) initcpio.write( ( s + '\n' ).toUtf8() );
+        initcpio.close();
+
+        out << "\n\nupdating initcpio...\n\n";
+        out.flush();
+        system("mkinitcpio -P");
+
+    }
+    return 0;
+}
+int setLog(){
+    QTextStream out( stdout );
+
+    if ( !( getuid()==0 ) ){
+        out << "you should use this command with sudo\n";
+        return 0;
+    }
+    QFile grub( "/etc/default/grub" );
+    if ( !grub.open( QFile::ReadWrite | QFile::Text ) )
+        return -1;
+    QStringList data = QString( grub.readAll() ).split( "\n" );
+
+    QString cmdline;
+
+    int position = data.indexOf( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT=.*") );
+    cmdline = data.at( position );
+
+    QStringList cmdlineList = cmdline.replace( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT="), "" )
+                                     .replace( "\"", "" )
+                                     .split( ' ' );
+
+    if ( cmdlineList.indexOf("quiet")!=-1 ){
+        cmdlineList.removeAll( "quiet" );
+        out << "removing \"quiet\"\n";
+    }
+
+    if ( data.indexOf( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT=.*") ) != -1 ){
+        cmdlineList.removeAt( cmdlineList.indexOf( QRegularExpression("bootsplash.bootfile=.*") ) );
+        out << "removing bootsplash...\n";
+    }
+
+    cmdline.clear();
+    cmdline="GRUB_CMDLINE_LINUX_DEFAULT=\"";
+    for ( const QString& s : qAsConst(cmdlineList) ) cmdline.append( s ).append( ' ' );
+    cmdline.replace( QRegularExpression( "\\s$" ), "\"");
+
+    data.replace( position, cmdline );
+
+    // write grub
+    out << "Writing GRUB...\nbackup will be saved to /etc/default/grub.bak\n\n";
+    out.flush();
+
+    system( "cp /etc/default/grub /etc/default/grub.bak" );
+    grub.resize( 0 );
+    for ( const QString& s : qAsConst(data) ) grub.write( ( s + '\n' ).toUtf8() );
+    grub.close();
+
+    data.clear();
+
+    out << "updating GRUB...\n\n";
+    out.flush();
+    system("grub-mkconfig -o /boot/grub/grub.cfg");
+    return 0;
+
+}
+int disable(){
+    QTextStream out( stdout );
+
+    if ( !( getuid()==0 ) ){
+        out << "you should use this command with sudo\n";
+        return 0;
+    }
+    QFile grub( "/etc/default/grub" );
+    if ( !grub.open( QFile::ReadWrite | QFile::Text ) )
+        return -1;
+    QStringList data = QString( grub.readAll() ).split( "\n" );
+
+    if ( data.indexOf( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT=.*") ) == -1 ){
+        out << "bootsplash is already disabled\n";
+        return 0;
+    }
+    out << "removing bootsplash...\n";
+
+    QString cmdline;
+
+    int position = data.indexOf( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT=.*") );
+    cmdline = data.at( position );
+
+    QStringList cmdlineList = cmdline.replace( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT="), "" )
+                                     .replace( "\"", "" )
+                                     .split( ' ' );
+
+    if ( cmdlineList.indexOf("quiet")==-1 )  cmdlineList.append( "quiet" );
+
+    cmdlineList.removeAt( cmdlineList.indexOf( QRegularExpression("bootsplash.bootfile=.*") ) );
+
+    cmdline.clear();
+    cmdline="GRUB_CMDLINE_LINUX_DEFAULT=\"";
+    for ( const QString& s : qAsConst(cmdlineList) ) cmdline.append( s ).append( ' ' );
+    cmdline.replace( QRegularExpression( "\\s$" ), "\"");
+
+    data.replace( position, cmdline );
+
+    // write grub
+    out << "Writing GRUB...\nbackup will be saved to /etc/default/grub.bak\n\n";
+    out.flush();
+
+    system( "cp /etc/default/grub /etc/default/grub.bak" );
+    grub.resize( 0 );
+    for ( const QString& s : qAsConst(data) ) grub.write( ( s + '\n' ).toUtf8() );
+    grub.close();
+
+    data.clear();
+
+    out << "updating GRUB...\n\n";
+    out.flush();
+    system("grub-mkconfig -o /boot/grub/grub.cfg");
+    return 0;
+}
+int list(){
+    QTextStream out( stdout );
+    QDir d( "/usr/lib/firmware/bootsplash-themes/" );
+    d.setFilter( QDir::NoDotAndDotDot | QDir::Dirs );
+    if ( d.count() == 0 )
+        out << "There are no installed themes\n";
+    else{
+        QStringList themes = d.entryList();
+        out << "Found " << themes.count() << " themes:\n";
+        for ( const QString& n : qAsConst(themes) ) out << n << "\n";
+    }
+    return 0;
+}
+int status(){
+    QTextStream out( stdout );
+
+    bool kernelFlag = 0, quietFlag=0, bootfileFlag=0, hooksFlag=0;
+
+    QProcess readconf;
+    readconf.start( "zgrep", QStringList() << "CONFIG_BOOTSPLASH" << "/proc/config.gz" );
+    readconf.waitForFinished();
+    kernelFlag = QString(readconf.readAllStandardOutput()) == "CONFIG_BOOTSPLASH=y\n";
+
+
+    QFile grub( "/etc/default/grub" );
+    if ( !grub.open( QFile::ReadOnly | QFile::Text ) )
+        return -1;
+    QString cmdline;
+    while ( !cmdline.contains( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT=") ) ) cmdline = grub.readLine();
+
+    QStringList cmdlineList = cmdline.replace( QRegularExpression("^GRUB_CMDLINE_LINUX_DEFAULT="), "" )
+                                     .replace( "\"", "" )
+                                     .split( ' ' );
+    QString theme;
+    for ( const QString& opt : qAsConst(cmdlineList) ){
+        if ( opt=="quiet" ) quietFlag=1;
+        if ( opt.contains( "bootsplash.bootfile=" ) ){
+            bootfileFlag=1;
+            theme=opt;
+        }
+    }
+    theme.replace( "bootsplash.bootfile=/bootsplash-themes/", "" ).replace( "/bootsplash", "" ).replace( "\n", "" );
+
+    QString ok = "[\033[42m\ ok \033[00m\]",
+          warn = "[\033[43m\warn\033[00m\]";
+
+    out << "Bootsplash status:\n"
+        << ( ( bootfileFlag&!theme.isEmpty() )?
+                   ok+" Bootsplash is enabled, current theme: "+theme+"\n"
+                :warn+" Bootsplash is disabled or theme is not set\n" )
+        << ( kernelFlag?
+                   ok+" Current kernel supports bootsplash\n"
+                :warn+" Seems like your current kernel doesn't have bootsplash support\n" );
+
+    if ( !theme.isEmpty() ){
+        QFile initcpio( "/etc/mkinitcpio.conf" );
+        if ( !initcpio.open( QFile::ReadOnly | QFile::Text ) )
+            return -1;
+        QString hooks;
+        while ( !hooks.contains( QRegularExpression("^HOOKS=") ) )
+            hooks = initcpio.readLine();
+        hooksFlag = hooks.contains( QRegularExpression("[\\s,\"]bootsplash-"+theme+"[\\s,\"]") );
+
+        out << ( hooksFlag?
+                   ok+" current theme is in hooks list\n"
+                :warn+" current theme not found in hooks list, you can add it manually or run:\nsudo bootsplash-manager -s "+theme+"\n" )
+            << ( !quietFlag?
+                   ok+" No \'quiet\' option\n"
+                :warn+" Found \'quiet\' option, you'll see black screen instead of bootsplash\n" );
+    }
+    return 0;
+}
+
