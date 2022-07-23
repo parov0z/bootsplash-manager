4f8310347bae12c690d2b406b187eaac0e8d684e
YetAnotherWorld <109851468+YetAnotherWorld@users.noreply.github.com>
2022-07-23 19:19:38 +0200

    Add french translation (#6)

---
+++ data/translations/bootsplash-manager_fr_FR.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>InstallDialog</name>
+    <message>
+        <location filename="../../src/gui/installdialog.ui" line="20"/>
+        <source>Install themes</source>
+        <translation>Installer des thèmes</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/installdialog.ui" line="44"/>
+        <source>enable AUR</source>
+        <translation>activer l&apos;AUR</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/installdialog.ui" line="67"/>
+        <source>Install</source>
+        <translation>Installer</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/installdialog.cpp" line="93"/>
+        <source>Themes to install: </source>
+        <translation>Thèmes à installer: </translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/installdialog.cpp" line="95"/>
+        <source>
+Continue?</source>
+        <translation>
+Continuer?</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/installdialog.cpp" line="107"/>
+        <source>Installing...
+</source>
+        <translation>Installation...
+</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/installdialog.cpp" line="108"/>
+        <source>This may take a few minutes</source>
+        <translation>Cela peut prendre quelques minutes</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/gui/mainwindow.cpp" line="22"/>
+        <source>No installed themes detected</source>
+        <translation>Pas de thèmes installés détectés</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/mainwindow.cpp" line="132"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="197"/>
+        <source>Please, wait...</source>
+        <translation>Attendez s&apos;il vous plaît...</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/mainwindow.cpp" line="183"/>
+        <source>Do you really want to remove
+</source>
+        <translation>Voulez-vous vraiment le supprimer
+</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/gui/bootsplash-manager-gui.cpp" line="34"/>
+        <source>Cannot detect kernel bootsplash support
+Is it a custom kernel?
+Themes will not work, but you can still enable/disable the displaying of the boot log</source>
+        <translation>Impossible de détecter le support bootsplash du noyau
+Est-ce un noyau custom?
+Les thèmes ne fonctionneront pas, mais vous pouvez toujours activer/désactiver l&apos;affichage du boot log</translation>
+    </message>
+</context>
+</TS>
